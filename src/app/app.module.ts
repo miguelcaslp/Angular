4504da030ffb5b80f765d58691fb70bebdbe45f6
miguelllcas <miguelcasly@gmail.com>
2022-10-29 13:40:09 +0200

    p6 angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NotesService } from './services/notes.service';
+import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+} from '@abacritt/angularx-social-login';
+import { LoginService } from './services/login.service';
+
+
 
 @NgModule({
   declarations: [
@@ -15,9 +22,30 @@ import { NotesService } from './services/notes.service';
     BrowserModule,
     AppRoutingModule,
     ButtonComponent,
-    NavbarComponent
+    NavbarComponent,
+   
+  
   ],
-  providers: [NotesService],
+  
+  providers: [NotesService,
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: true,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '249018397259-qn0co2qpbq11uobs32td19bqfbirpfbh.apps.googleusercontent.com'
+            )
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    },LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [CommonModule,
-            RouterModule],
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+selector: 'app-navbar',
+standalone: true,
+imports: [CommonModule,RouterModule],
+templateUrl: './navbar.component.html',
+styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+constructor(public loginS:LoginService) { }
+ngOnInit(): void {
+}
+public logout(){
+this.loginS.signOut();
+}
 }
